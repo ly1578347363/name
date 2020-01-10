2b333bd206f8d617ef8f29001d5d4422021530a5
ly1578347363 <1578347363@qq.com>
2020-01-10 11:15:47 +0800

    第一次提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,28 @@
 import Vue from 'vue'
+//vue-router路由
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
 Vue.use(VueRouter)
 
+import login from '@/components/login.vue'
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // }
+
+    {
+        path: '/login',
+        component: login
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
